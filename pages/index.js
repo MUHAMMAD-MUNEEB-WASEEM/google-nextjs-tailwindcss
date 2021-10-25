81179ad3b3becd9df2476af3b95e89974ace9eab
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-10-25 16:38:57 +0500

    Body section completed!

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
+import Image from 'next/image'
 
-import { ViewGridIcon } from '@heroicons/react/solid'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon} from '@heroicons/react/outline'
+
 
 export default function Home() {
   return (
@@ -14,7 +17,7 @@ export default function Home() {
 
       {/*HEader*/ }
 
-      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+      <header className="flex w-full p-5 mb-20 justify-between text-sm text-gray-700">
 
         
         {/*left*/}
@@ -37,6 +40,30 @@ export default function Home() {
       </header>
 
       {/*Body*/ }
+      <form className="flex flex-col items-center justify-center content-center ">
+        <Image 
+        loading="lazy"
+        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+        height={100}
+        width={300}
+        />
+
+        <div className="flex w-full items-center mt-5 hover:shadow-lg focus-within:shadow-lg 
+        max-w-md rounded-full border-gray-200 px-5 py-3 sm:max-w-xl lg:max-w-2xl">
+         
+          <SearchIcon className="h-5 mr-3 text-gray-500"/>
+          <input className="flex-grow focus:outline-none"/>
+          <MicrophoneIcon className="h-5"/>
+        
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+
+
+      </form>
       {/*Footer*/ }
 
 
