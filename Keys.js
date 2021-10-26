58fe9008834ed5536d6b70fa9d11630acbf34063
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-10-26 19:00:07 +0500

    Done with search page functionality and ui

---
+++ Keys.js
@@ -0,0 +1,17 @@
+//API key
+
+    //to used api services
+    //https://developers.google.com/custom-search/v1/using_rest
+
+export const API_KEY = 'AIzaSyAy_yzwh_Oz4ojTjScxtt87Qf58EbPR9jM'
+
+
+//context key
+
+//Content can be created by visiting below link 
+
+    // https://cse.google.com/cse/create/new
+
+export const CONTEXT_KEY = '0e7ae6c2c3008a199'
+
+
